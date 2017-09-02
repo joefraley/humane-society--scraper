b3449a4cc1e9f501cb2f3a37adad25981a80be68
joefraley <jose.fraley@gmail.com>
2017-09-01 18:34:16 -0700

    ci(ALL): includes eslint config

--- src/index.js
+++ src/index.js
@@ -1,67 +1,67 @@
 // @flow
 
-const { get } = require('axios')
-const cheerio = require('cheerio')
-const {buffer, text, json, send} = require('micro')
-const {compose, toLower} = require('ramda')
-const dates = require('date-fns')
+const { get } = require("axios");
+const cheerio = require("cheerio");
+const { compose, toLower } = require("ramda");
 const {
   parseAge,
   parseColor,
   parseCurrency,
   parseDateAvailable,
   parseDescription,
-  parseWeight,
-} = require('./utils')
+  parseWeight
+} = require("./utils");
 
-
-const BASE_URL = 'https://www.oregonhumane.org'
-const ALL_ANIMALS_URL = `${BASE_URL}/adopt?type=all`
+const BASE_URL = "https://www.oregonhumane.org";
+const ALL_ANIMALS_URL = `${BASE_URL}/adopt?type=all`;
 
 const selectors = {
-  ANIMAL_LINK: '.animal-results .result-item a',
-  DESCRIPTION: '.detail-desc',
-  DETAILS: '.animal-details',
-  IMAGE_URL: '.detail-image img',
-  STATS: '.detail-text',
-  TABLE: 'table tbody',
-}
+  ANIMAL_LINK: ".animal-results .result-item a",
+  DESCRIPTION: ".detail-desc",
+  DETAILS: ".animal-details",
+  IMAGE_URL: ".detail-image img",
+  STATS: ".detail-text",
+  TABLE: "table tbody"
+};
 
-const fetchAllAnimals = async () =>
-    await get(ALL_ANIMALS_URL)
+const fetchAllAnimals = async () => await get(ALL_ANIMALS_URL);
 
-module.exports =
-  async (request, response) => {
-    const { data } = await fetchAllAnimals()
-    const $ = cheerio.load(data)
+module.exports = async () => {
+  const { data } = await fetchAllAnimals();
+  const $ = cheerio.load(data);
 
-    const urls =
-      $(selectors.ANIMAL_LINK)
-        .map((i, e) => $(e).attr('href'))
+  const urls = $(selectors.ANIMAL_LINK).map((i, e) => $(e).attr("href"));
 
-    const detailUrl = `${BASE_URL}${urls[0]}`
-    const { data: detail } = await get(detailUrl)
+  const detailUrl = `${BASE_URL}${urls[0]}`;
+  const { data: detail } = await get(detailUrl);
 
-    const $$ = cheerio.load(detail)
-    const details = $$(selectors.DETAILS)
-    const stats = details.find(selectors.STATS)
-    const table = stats.find(selectors.TABLE).children().toArray()
-    const parseDetail = toLower(index => $$(table[index]).find('td').text())
+  const $$ = cheerio.load(detail);
+  const details = $$(selectors.DETAILS);
+  const stats = details.find(selectors.STATS);
+  const table = stats
+    .find(selectors.TABLE)
+    .children()
+    .toArray();
+  const parseDetail = toLower(index =>
+    $$(table[index])
+      .find("td")
+      .text()
+  );
 
-    return {
-      adoptFee: compose(parseCurrency, parseDetail)(1),
-      age: compose(parseAge, parseDetail)(6),
-      animalType: parseDetail(2),
-      breed: parseDetail(3),
-      color: compose(parseColor, parseDetail)(5),
-      dateAvailable: compose(parseDateAvailable, parseDetail)(0),
-      description: parseDescription(details.find(selectors.DESCRIPTION).text()),
-      id: parseDetail(10),
-      imageUrl: details.find(selectors.IMAGE_URL).attr('src'),
-      kennel: parseDetail(9),
-      location: parseDetail(8),
-      name: stats.find('h2').text(),
-      sex: parseDetail(4),
-      weight: compose(parseWeight, parseDetail)(7),
-    }
-  }
+  return {
+    adoptFee: compose(parseCurrency, parseDetail)(1),
+    age: compose(parseAge, parseDetail)(6),
+    animalType: parseDetail(2),
+    breed: parseDetail(3),
+    color: compose(parseColor, parseDetail)(5),
+    dateAvailable: compose(parseDateAvailable, parseDetail)(0),
+    description: parseDescription(details.find(selectors.DESCRIPTION).text()),
+    id: parseDetail(10),
+    imageUrl: details.find(selectors.IMAGE_URL).attr("src"),
+    kennel: parseDetail(9),
+    location: parseDetail(8),
+    name: stats.find("h2").text(),
+    sex: parseDetail(4),
+    weight: compose(parseWeight, parseDetail)(7)
+  };
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const parseDate = require('date-fns/parse')
+const parseDate = require("date-fns/parse");
 const {
   compose,
   equals,
@@ -9,28 +9,28 @@ const {
   replace,
   split,
   toLower,
-  trim,
-} = require('ramda')
+  trim
+} = require("ramda");
 
-const parseDateValue = key => list =>  {
-  const labelIndex = findIndex(equals(key))(list)
-  const value = nth(labelIndex - 1)(list)
-  return {label: key, value: parseInt(value) || 0}
-}
+const parseDateValue = key => list => {
+  const labelIndex = findIndex(equals(key))(list);
+  const value = nth(labelIndex - 1)(list);
+  return { label: key, value: parseInt(value) || 0 };
+};
 
-const dropNonNumbers = replace(/[^0-9\.]+/g, "")
+const dropNonNumbers = replace(/[^0-9\.]+/g, ""); // eslint-disable-line no-useless-escape
 
 module.exports = {
   parseAge: compose(
     list => ({
-      years: prop('value')(parseDateValue('years')(list)),
-      months: prop('value')(parseDateValue('months')(list)),
+      years: prop("value")(parseDateValue("years")(list)),
+      months: prop("value")(parseDateValue("months")(list))
     }),
-    split(' ')
+    split(" ")
   ),
-  parseColor: compose(map(trim), split(','), toLower),
+  parseColor: compose(map(trim), split(","), toLower),
   parseCurrency: compose(parseFloat, dropNonNumbers),
   parseDateAvailable: parseDate,
   parseDescription: trim,
-  parseWeight: string => parseFloat(string) || 0,
-}
+  parseWeight: string => parseFloat(string) || 0
+};
