10ef1e040186b6b40fc830f8feb07e3ad08d670a
joefraley <joefraley@protonmail.com>
2017-09-06 11:04:32 -0700

    test(routes): drop request-promise in favor of axios
    i was already using axios in the route code. may as well stay consistent. i only used
    request-promise because it was copy-pasta from the micro doccs
    
    closes #32

--- test/server.js
+++ test/server.js
@@ -1,14 +1,10 @@
 import { Animal } from "../src/schema";
+import { get } from "axios";
 import { list } from "tcomb";
-import { merge } from "ramda";
 import listen from "test-listen";
 import main from "../src/index";
 import micro from "micro";
-import rp from "request-promise";
 import test from "ava";
-
-const request = options =>
-  rp(merge({ resolveWithFullResponse: true, json: true }, options));
 
 test.beforeEach(spec => (spec.context.service = micro(main)));
 
@@ -16,20 +12,18 @@ test.afterEach(spec => spec.context.service.close());
 
 test("GET: / - sends back a list of animals", async assert => {
   const uri = await listen(assert.context.service);
-  const { body, statusCode } = await request({
-    uri,
-    qs: { count: 1 }
+  const { data, status } = await get(uri, {
+    params: { count: 1 }
   });
-  assert.is(statusCode, 200);
-  assert.notThrows(() => list(Animal)(body));
+  assert.is(status, 200);
+  assert.notThrows(() => list(Animal)(data));
 });
 
 test("GET: / - respects the desired count", async assert => {
   const uri = await listen(assert.context.service);
-  const { body, statusCode } = await request({
-    uri,
-    qs: { count: 1 }
+  const { data, status } = await get(uri, {
+    params: { count: 1 }
   });
-  assert.false(body.length > 1);
-  assert.is(statusCode, 200);
+  assert.false(data.length > 1);
+  assert.is(status, 200);
 });
